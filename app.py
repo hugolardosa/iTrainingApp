4eeb409d97c4e97ba1d50db1ae73e508f8f44145
Hugo <hugolardosa@gmail.com>
2021-01-05 15:19:10 +0000

    base

--- app.py
+++ app.py
@@ -3,15 +3,16 @@ from flask_login import LoginManager, UserMixin, login_required, current_user, l
 from werkzeug.security import generate_password_hash, check_password_hash
 from DataTypes import *
 
-## 
+##### Clients and PT for demo ##############
 demo_cl = Client(email="clarice@gmail.com", name="Clarice", password=generate_password_hash("123", method='sha256'), address="Rua dos ovos moles", city="Aveiro", cell_phone="999999999", postal_code="2330-555", bday="12-12-1995", weight="120", height="123", obj="Perder peso", health_problems="Nenhum")
 demo_pt = Pt(email="cc@gmail.com", name="Calisto Comum", password=generate_password_hash("123", method='sha256'), pt_code="!THE_PT_CODE_123!", address="Rua dos ovos moles", city="Aveiro", cell_phone="919191911", postal_code="2330-555")
 demo_cl2 = Client(email="roberto@gmail.com", name="Roberto", password=generate_password_hash("123", method='sha256'), address="Bairro do liceu", city="Aveiro", cell_phone="911222333", postal_code="2330-555", bday="12-12-1995", weight="120", height="123", obj="Perder peso", health_problems="Nenhum")
 demo_cl2.train_list = [Trains("Braços", "1-12-2020", "5", [Exercice("Elevações na cadeira", "10")])]
+#############
 
-
+# list with users from app
 users = [demo_cl, demo_pt, demo_cl2]
-print(users)
+
 
 app = Flask(__name__,
             static_folder='static',
@@ -43,7 +44,7 @@ def login_post():
     # check if the user actually exists
     # take the user-supplied password, hash it, and compare it to the hashed password in the database
     if not user or not check_password_hash(user.password, password):
-            flash('Please check your login details and try again.')
+            flash('O e-mail ou a password está errada. Tente novamente.')
             return redirect(url_for('login')) # if the user doesn't exist or password is wrong, reload the page
 
     # if the above check passes, then we know the user has the right credentials
@@ -107,7 +108,7 @@ def profile():
 def calendar():
     return render_template('calendar.html')
 
-@app.route('/train')
+@app.route('/train')  #/train?id=
 def train():
     train_id = int(request.args.get("id"))
     current_user.train_list[train_id].done = True
